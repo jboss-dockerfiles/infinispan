64c495b1cce833b666005e8a7c512de701e82b2f
gustavonalle <gustavonalle@gmail.com>
2016-10-17 17:09:55 +0100

    Avoid adding duplicate attributes in the config file

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -19,7 +19,9 @@ then
   shift;
   LAUNCHER=$SERVER/bin/domain.sh
   BASE=$(echo -n $MGMT_USER | base64) && sed -i "s/\<secret value=.*/secret value=\"$BASE\" \/>/" $SERVER/domain/configuration/host-slave.xml
-  sed -i  "s/\(remote security-realm=\"ManagementRealm\"\)/\1 username=\"$MGMT_USER\"/" $SERVER/domain/configuration/host-slave.xml
+  SLAVE_FILE=$SERVER/domain/configuration/host-slave.xml
+  sed -e "/<remote/ {/username/! s/remote/remote username=\"$MGMT_USER\"/}" -i $SLAVE_FILE
+  sed -e "/<remote.*username/ s/\(username\)=\"[^\"]*\"/username=\"$MGMT_USER\"/" -i $SLAVE_FILE
   exec $LAUNCHER --host-config host-slave.xml -Djboss.domain.master.address=$DC_PORT_9990_TCP_ADDR $BIND_OPTS "$@"
 else
   if [ $# -ne 0 ] && [ -f $SERVER/standalone/configuration/$1.xml ]; then CONFIG=$1; shift; fi
