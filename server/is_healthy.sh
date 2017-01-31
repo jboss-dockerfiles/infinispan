0f4a08cde998d63be15e5eb4e53efa9b51f45d01
Sebastian Laskawiec <slaskawi@redhat.com>
2017-01-31 13:24:59 +0100

    ISPN-6679 Added scripts for Health Check API

---
+++ server/is_healthy.sh
@@ -0,0 +1,4 @@
+
+/opt/jboss/infinispan-server/bin/ispn-cli.sh -c --controller=$(hostname -i):9990 '/subsystem=datagrid-infinispan/cache-container=clustered/health=HEALTH:read-attribute(name=cluster-health)' | grep HEALTHY
+
+exit $?
